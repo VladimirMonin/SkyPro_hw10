d78b4eb78b7e42735441e389bbe7fea3cbad1026
797888260286@yandex.ru <AN7H5QwEwBHHTDs>
2022-03-10 14:32:32 +0600

    chore: Добавил функцию load_candidates

--- app.py
+++ app.py
@@ -1,3 +1,17 @@
 from flask import Flask
+import json
 
-#
+def load_candidates(filename='candidates.json'):
+    """
+    Функция читает json с вопросами и возвращает словарь для игры
+    :param filename: По умолчанию questions.json
+    :return: Словарь
+    """
+    file = open(filename, encoding='UTF-8')
+    candidates_list = json.load(file)
+    file.close()
+    return candidates_list
+
+
+# ЗАГРУЗКА ВОПРОСОВ
+candidates_list = load_candidates()
